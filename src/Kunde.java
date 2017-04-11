1232d34a94bafc3a2998966536e247572ea14ffb
WahidVanaki <wahidvanaki@10.60.34.253>
2017-04-11 15:39:06 +0200

    neuer Eintrag

--- src/Kunde.java
+++ src/Kunde.java
@@ -3,11 +3,20 @@ public class Kunde {
 
 	
 private long id = 0L;
+private String name;
 
 	public long getID(){
 		return this.id;
 	}
-	
-	
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	}
 }
